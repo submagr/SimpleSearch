f71e5c19edfded3fd79da4d85954b557c2b395a4
SaloniGoyal <SaloniGoyal@users.noreply.github.com>
2017-08-14 12:37:58 +0530

    Update ValidateQuery.cpp

--- SimpleSearch/ValidateQuery.cpp
+++ SimpleSearch/ValidateQuery.cpp
@@ -1,25 +1,9 @@
  #include "ValidateQuery.h"
-
- void static ValidateQuery::CreateDictionary()
-{
-    std::vector< string > words;
-    string line;
-    ifstream myfile ("words.txt");
-    if (myfile.is_open())
-    {
-        while ( getline (myfile,line) )
-        {
-            words.push_back(line);
-        }
-        myfile.close();
-        trie.build_trie(words);
-    }
-    else cout << "Unable to open file";     
-}
+#include "common.h"
 
 ValidateQuery::ValidateQuery(string query){
     CreateDictionary();
     bool in_trie = false;
-    trie.search(query, in_trie);
+    dictionaryTrie.search(query, in_trie);
     cout << query << in_trie << endl;
 }
