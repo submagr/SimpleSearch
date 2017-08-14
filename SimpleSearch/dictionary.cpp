98a522e63475292d3fd83b74351b4032b71ce5ca
SaloniGoyal <SaloniGoyal@users.noreply.github.com>
2017-08-14 12:36:01 +0530

    Add files via upload

---
+++ SimpleSearch/dictionary.cpp
@@ -0,0 +1,21 @@
+ #include "Dictionary.h"
+ #include "common.h"
+
+ Dictionary::Dictionary()
+{
+    Trie dictionaryTrie;
+    std::vector< string > words;
+    string line;
+    ifstream myfile ("words.txt");
+    if (myfile.is_open())
+    {
+        while ( getline (myfile,line) )
+        {
+            words.push_back(line);
+        }
+        myfile.close();
+        dictionaryTrie.build_trie(words);
+    }
+    else cout << "Unable to open file";     
+}
+
