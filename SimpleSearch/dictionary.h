ebe3fa19231ac17c7f6039462ad371077089ad53
SaloniGoyal <SaloniGoyal@users.noreply.github.com>
2017-08-14 12:34:20 +0530

    Add files via upload

---
+++ SimpleSearch/dictionary.h
@@ -0,0 +1,18 @@
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class Dictionary{
+
+public:
+    Dictionary();
+ 
+};
+
+#endif
